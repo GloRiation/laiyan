1a0039cdca4897f887edea028f364ef3624d930b
GloRiation <ivanyiu2017@gmail.com>
2020-09-04 10:36:20 +0800

    added sth

---
+++ php/test.php
@@ -0,0 +1,16 @@
+<?php
+$t = 20;
+if($t < 20){
+	echo "Have a good day!";
+}
+else{
+	echo "Have a good night!";
+}
+?>
+
+
+<html>
+	
+	<head></head>
+	<body></body>
+</html>
---
+++ php/test2.php
@@ -0,0 +1,13 @@
+<?php
+$a = "1";
+
+switch($a){
+	case 1: 
+		print "Passed";
+		break;
+	case 2: 
+		print "Failed";
+		break;
+	default: print "Absent";
+}
+?>
